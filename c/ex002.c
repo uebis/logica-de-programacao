535b3659e93e1052edc343e7cb7c620e6fb4172e
Andrezza <37053295+uebis@users.noreply.github.com>
2023-04-09 12:56:24 -0300

    exercício: horário do cinema

--- c/ex002.c
+++ c/ex002.c
@@ -5,4 +5,20 @@
 int main ()
 {
     setlocale(LC_ALL, "Portuguese_Brazil");
+    int hora_cinema = 20;
+    int hora_atual = 20;
+    int hora_validada = hora_cinema + 30;
+
+    //Posso entrar no cinema?
+
+    if  (hora_atual > hora_validada) {
+        printf("Passou do tempo limite! Não pode entrar");
+    }else if(hora_atual < hora_cinema - 30) {
+        printf("Nem abriu a porta do cinema ainda");
+    }else{
+        printf("O horário está certo! Pode entrar!");
+    }
+
+    return 0;
+
 }
